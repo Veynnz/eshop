fa15fac274bea198c345edfafbc9a5afb7046a0b
Dion <diodavis123@gmail.com>
2025-02-19 20:39:31 +0700

    Editing gradle build to add sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Veynnz_eshop")
+        property("sonar.organization", "veynnz")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "id.ac.ui.cs.advprog"
