46c5bd282d9834944434be4ef8aed03d568c0ded
Dion <diodavis123@gmail.com>
2025-03-06 16:09:36 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,61 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+    private Order order;
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp(){
+        paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+    }
+
+    @Test
+    void testCreatePayment(){
+        Payment payment = new Payment("a1b2c3d4-e5f6-7890-1234-567890abcdef", "VOUCHER", paymentData);
+        assertEquals("a1b2c3d4-e5f6-7890-1234-567890abcdef", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals(paymentData, payment.getPaymentData());
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentInvalidMethod() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("a1b2c3d4-e5f6-7890-1234-567890abcdef", "RAWR",
+                    paymentData);
+        });
+    }
+
+    @Test
+    void testCreatePaymentInvalidPaymentData() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Payment("a1b2c3d4-e5f6-7890-1234-567890abcdef", "VOUCHER", null);
+        });
+    }
+
+    @Test
+    void testSetStatusToRejected() {
+        Payment payment = new Payment("a1b2c3d4-e5f6-7890-1234-567890abcdef", "VOUCHER", paymentData);
+        payment.setStatus("REJECTED");
+
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusToInvalid() {
+        Payment payment = new Payment("a1b2c3d4-e5f6-7890-1234-567890abcdef", "VOUCHER", paymentData);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment.setStatus("RAWR RAWR");
+        });
+    }
+}
